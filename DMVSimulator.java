9a5e138c40760df3129b013e9d3cd5c9d6e65ab0
Kmschro <klaus072004@gmail.com>
2023-03-01 14:57:10 -0500

    Change Wait Time

--- DMVSimulator.java
+++ DMVSimulator.java
@@ -3,7 +3,7 @@ class DMVSimulator {
     public static void main( String args[] ) {
         System.out.println("Welcome to the DMV");
         Random rand = new Random(); 
-        int upperbound = 100;
+        int upperbound = 200;
         int int_random = rand.nextInt(upperbound); 
         System.out.println("Your number is " + int_random + " and you will have to wait until your number is called.");
         for (int i = 1; i <= int_random; i++){
