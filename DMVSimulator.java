4f5d757def81ed37b433b24882c332a27007d49d
Kmschro <klaus072004@gmail.com>
2023-03-01 14:37:54 -0500

    Blank Master Branch

--- DMVSimulator.java
+++ DMVSimulator.java
@@ -1,12 +1,6 @@
 import java.util.Random;
 class DMVSimulator {
     public static void main( String args[] ) {
-      System.out.println("Welcome to the DMV");
-      Random rand = new Random(); 
-      int upperbound = 100;
-      int int_random = rand.nextInt(upperbound); 
-      for (int i = 1; i <= int_random; i++){
-            System.out.println(i);
-        }
+      
     }
 }
